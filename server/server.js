a408b8adaec51ddc192a49268efc21dd8690855a
Pat Jensen <pjensen@aclu.org>
2018-08-02 21:01:09 -0400

    Create generateMessage utility.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,30 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('connection made!');
 
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: `Welcome to the chat.`,
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', `Welcome to the chat.`));
 
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: `New user joined.`,
-        createdAt: new Date().getTime()
-    })
+    socket.broadcast.emit('newMessage', generateMessage('Admin', `New user joined.`));
 
     socket.on('createMessage', (message) => {
         console.log('create message:', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
-        // socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // });
+        io.emit('newMessage', generateMessage(message.from, message.text));
     });
 });
 
