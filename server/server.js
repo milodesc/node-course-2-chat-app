c86ff6b6afed9cdd8419910affaf09ff02534e15
Pat Jensen <pjensen@aclu.org>
2018-08-06 13:10:27 -0400

    Add linting and fix up server side code.

--- server/server.js
+++ server/server.js
@@ -3,35 +3,29 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
-const {generateLocationMessage} = require('./utils/message');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-var app = express();
-var server = http.createServer(app);
-var io = socketIO(server);
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('connection made!');
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat.'));
 
-    socket.emit('newMessage', generateMessage('Admin', `Welcome to the chat.`));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined.'));
 
-    socket.broadcast.emit('newMessage', generateMessage('Admin', `New user joined.`));
+  socket.on('createMessage', (message, callback) => {
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback();
+  });
 
-    socket.on('createMessage', (message, callback) => {
-        console.log('create message:', message);
-        io.emit('newMessage', generateMessage(message.from, message.text));
-        callback();
-    });
-
-    socket.on('createLocationMessage', (coords) => {
-        console.log(coords);
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
-    })
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
 });
 
-server.listen(port, () => {
-    console.log(`Server started on port ${port}.`);
-});
+server.listen(port, () => {});
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,27 +1,27 @@
-var expect = require('expect');
+const expect = require('expect');
 
-var {generateMessage, generateLocationMessage} = require('./message');
+const { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
-    it('should generate correct message object', () => {
-        var from = "Pat";
-        var text = "Some message";
-        var message = generateMessage(from, text);
+  it('should generate correct message object', () => {
+    const from = 'Pat';
+    const text = 'Some message';
+    const message = generateMessage(from, text);
 
-        expect(message.createdAt).toBeA('number');
-        expect(message).toInclude({from, text});
-    });
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({ from, text });
+  });
 });
 
 describe('generateLocationMessage', () => {
-    it('should generate correct location object', () => {
-        var from = "Pat";
-        var latitude = 39;
-        var longitude = 40;
-        var url = `https://www.google.com/maps?q=${latitude},${longitude}`;
-        var message = generateLocationMessage(from, latitude, longitude);
+  it('should generate correct location object', () => {
+    const from = 'Pat';
+    const latitude = 39;
+    const longitude = 40;
+    const url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+    const message = generateLocationMessage(from, latitude, longitude);
 
-        expect(message.createdAt).toBeA('number');
-        expect(message).toInclude({from, url});
-    });
-})
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({ from, url });
+  });
+});
